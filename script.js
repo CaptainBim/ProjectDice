f9191d568a8087d8c9c4c11fec9f3ca61935bc68
Bim <bimowashere@gmail.com>
2023-10-10 13:38:15 +0800

    hmm
    template

---
+++ script.js
@@ -0,0 +1,5 @@
+/*
+    1. Tuliskan seluruh kode Java Script Anda di sini
+    2. Semua file di project ini TIDAK PERLU diubah, selain file ini
+    3. Pastikan program anda rapi, terstruktur dengan baik, dan mudah dibaca
+*/
